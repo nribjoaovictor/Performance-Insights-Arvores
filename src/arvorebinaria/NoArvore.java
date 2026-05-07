59a34dac65b6c69e3d479e340557aa094111470c
Joao Victor Nascimento <nrib.joaovictor@gmail.com>
2026-05-07 15:27:30 -0300

    nó da arvore

--- src/arvorebinaria/NoArvore.java
+++ src/arvorebinaria/NoArvore.java
@@ -1,4 +1,46 @@
 package arvorebinaria;
 
-public class NoArvore {
+public class NoArvore<T> {
+    private T valor;
+    private NoArvore<T> esquerda;
+    private NoArvore<T> direita;
+    private int Altura;
+
+
+    public NoArvore(T valor){
+       this.valor = valor;
+       this.Altura = 0;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public NoArvore<T> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(NoArvore<T> direita) {
+        this.direita = direita;
+    }
+
+    public NoArvore<T> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(NoArvore<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int altura) {
+        Altura = altura;
+    }
 }
